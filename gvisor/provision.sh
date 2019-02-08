aa3c99421dbc00c44ab4d2c8ae1c698f631d3630
Gosuke Miyashita <gosukenator@gmail.com>
2019-02-08 21:25:32 +0900

    Remove unneeded lines

--- gvisor/provision.sh
+++ gvisor/provision.sh
@@ -1,25 +1,6 @@
 #!/bin/sh
 
 apt-get update
-apt-get -y install gcc make pkg-config libseccomp-dev
-
-if [ ! -f go1.11.5.linux-amd64.tar.gz ]; then
-    curl -s -O https://dl.google.com/go/go1.11.5.linux-amd64.tar.gz
-    tar -C /usr/local -xzf go1.11.5.linux-amd64.tar.gz
-    echo 'export PATH=$PATH:/usr/local/go/bin' >> /etc/profile
-fi
-
-GOPATH=/root/go
-if [ ! -d $GOPATH ]; then
-    mkdir -p $GOPATH/src/github.com/opencontainers
-fi
-
-if [ ! -f /usr/local/sbin/runc ]; then
-    go get github.com/opencontainers/runc
-    cd $GOPATH/src/github.com/opencontainers/runc
-    make
-    make install
-fi
 
 if [ ! -x /usr/local/bin/runsc ]; then
     wget https://storage.googleapis.com/gvisor/releases/nightly/latest/runsc
