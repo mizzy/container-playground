2f6a658ef81c64b82f02945a3cb516666d7a128e
Gosuke Miyashita <gosukenator@gmail.com>
2019-03-06 10:18:53 +0900

    Use kata containers version 1.5 instead of master(1.6rc1)

--- measurements/provision.sh
+++ measurements/provision.sh
@@ -71,7 +71,7 @@ fi
 if [ ! -x /usr/bin/kata-runtime ]; then
     ARCH=$(arch)
     sh -c \
-       "echo 'deb http://download.opensuse.org/repositories/home:/katacontainers:/releases:/${ARCH}:/master/xUbuntu_$(lsb_release -rs)/ /' \
+       "echo 'deb http://download.opensuse.org/repositories/home:/katacontainers:/releases:/${ARCH}:/stable-1.5/xUbuntu_$(lsb_release -rs)/ /' \
   > /etc/apt/sources.list.d/kata-containers.list"
     curl -sL \
          http://download.opensuse.org/repositories/home:/katacontainers:/releases:/${ARCH}:/master/xUbuntu_$(lsb_release -rs)/Release.key \
