a3db087f64e71b4110375ef4ccac8c96bfa0d92f
Gosuke Miyashita <gosukenator@gmail.com>
2019-03-16 21:01:01 +0900

    Update measuring script

--- measurements/measure_startup_time.sh
+++ measurements/measure_startup_time.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
 
+pushd bundle
+
+cp config.json.for_non_nabla config.json
+
 # runc
 echo "##### runc"
-time sudo runc run mycontainer
+time sudo runc run bundle
 echo
 
 # gVisor
 echo "##### gVisor"
-time sudo runsc -log /dev/null run mycontainer
+time sudo runsc -log /dev/null run bundle
 echo
 
 # Kata Containers
 echo "##### Kata Containers"
-time sudo kata-runtime run mycontainer
+time sudo kata-runtime run bundle
 
-time sudo runnc create abcdefg123456 && sudo runnc start abcdefg123456
+cp config.json.for_nabla config.json
 
+# Nabla Containers
+# https://github.com/nabla-containers/runnc/issues/53
+sudo runnc create abcdefg123456 && sudo runnc --debug start abcdefg123456
+
+sudo runnc delete abcdefg123456
+
+popd
