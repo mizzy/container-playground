00de90ee65c6a896d4abf5982877ef53cfa4b55c
Gosuke Miyashita <gosukenator@gmail.com>
2019-03-17 14:30:23 +0900

    Update firecracker provisioning

--- measurements/provision/firecracker.sh
+++ measurements/provision/firecracker.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
 
 if [ ! -x /usr/local/bin/firecracker ]; then
+    sudo apt install -y musl-tools
+    curl https://sh.rustup.rs -sSf | sh -s -- -y
+    export PATH=$PATH:$HOME/.cargo/bin
+    rustup target add x86_64-unknown-linux-musl
+
     git clone https://github.com/firecracker-microvm/firecracker
     cd firecracker
-    tools/devtool -y build
-    mv build/debug/firecracker /usr/local/bin
+    git checkout v0.12.0
+    cargo build --release --features vsock
+    cp target/x86_64-unknown-linux-musl/release/{firecracker,jailer} \
+       /usr/local/bin
+    
+    setfacl -m u:vagrant:rw /dev/kvm
 fi
